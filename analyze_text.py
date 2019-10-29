111ece47cd28e92cb4b6f86304ecacc15de26c90
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-10-29 17:53:52 -0400

    update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -12,6 +12,16 @@ def open_webpage(page_name):
     return content
 
 
+def webpage_content(page_name):
+    """
+    open the desired summary of any webpage from wikipeda
+    """
+    wiki = MediaWiki()
+    page = wiki.page(page_name)
+    summary = page.summary
+
+    return summary
+
 #need to install nltk on command prompt
 import nltk
 from nltk.tokenize import word_tokenize
@@ -100,16 +110,6 @@ def top_10_words(hist, num = 10):
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 # nltk.download('averaged_perceptron_tagger')
 
-def webpage_content(page_name):
-    """
-    open the desired summary of any webpage from wikipeda
-    """
-    wiki = MediaWiki()
-    page = wiki.page(page_name)
-    summary = page.summary
-
-    return summary
-
 def nltk_sentiment_analyzer(summary):
     """
     checking the polarity of the summary portion of the webpage
@@ -119,23 +119,71 @@ def nltk_sentiment_analyzer(summary):
         
 
 def main():
-    text = open_webpage("Wikipedia")
-    summary = webpage_content("Wikipedia")
-    hist = tokenize_words(text)
-    freq = word_freq(hist)
+    #python
+    content_1 = open_webpage("Python (programming language)")
+    summary_1 = webpage_content("Python (programming language)")
+    hist_1 = tokenize_words(content_1)
+    freq_1 = word_freq(hist_1)
 
-    print("Characterizing by Word Frequencies:")
-    print_freq_list(freq)
+    #R
+    content_2 = open_webpage("R (programming language)")
+    summary_2 = webpage_content("R (programming language)")
+    hist_2 = tokenize_words(content_2)
+    freq_2 = word_freq(hist_2)
 
+    #C++
+    content_3 = open_webpage("C (programming language)")
+    summary_3 = webpage_content("C (programming language)")
+    hist_3 = tokenize_words(content_3)
+    freq_3 = word_freq(hist_3)
+    print(content_3)
+
+    #python analysis
+    print("This is the Text Analysis on the Python WikiPage:")
+    # print("Python WikiPage Characterized by Word Frequencies:")
+    # print_freq_list(freq_1)
+    # print('\n')
+
+    print("The Top 10 Most Frequent Words on the Python WikiPagge is:")
+    top_10_words(freq_1)
     print('\n')
 
-    print("The Top 10 Most Frequent Words on this Wikipedia Page is:")
-    top_10_words(freq)
+    print("Sentiment Analysis of the Summary Portion of the Python WikiPage:")
+    nltk_sentiment_analyzer(summary_1)
+    print("Sentiment Analysis of the Content Portion of the Python WikiPage:")
+    nltk_sentiment_analyzer(content_1)
+
+
+    #R analysis
+    print("This is the Text Analysis on the R WikiPage:")
+    # print("R WikiPage Characterized by Word Frequencies:")
+    # print_freq_list(freq_2)
+    # print('\n')
+
+    print("The Top 10 Most Frequent Words on the R WikiPagge is:")
+    top_10_words(freq_2)
+    print('\n')
+
+    print("Sentiment Analysis of the Summary Portion of the R WikiPage:")
+    nltk_sentiment_analyzer(summary_2)
+    print("Sentiment Analysis of the Content Portion of the R WikiPage:")
+    nltk_sentiment_analyzer(content_2)
+
     
+    #C analysis
+    print("This is the Text Analysis on the C WikiPage:")
+    # print("C WikiPage Characterized by Word Frequencies:")
+    # print_freq_list(freq_3)
+    # print('\n')
+
+    print("The Top 10 Most Frequent Words on the C WikiPagge is:")
+    top_10_words(freq_3)
     print('\n')
 
-    print("Sentiment Analysis of the Summary Portion of the Wikipedia Page:")
-    nltk_sentiment_analyzer(summary)
+    print("Sentiment Analysis of the Summary Portion of the C WikiPage:")
+    nltk_sentiment_analyzer(summary_3)
+    print("Sentiment Analysis of the Content Portion of the C WikiPage:")
+    nltk_sentiment_analyzer(content_3)
 
 
 if __name__ == '__main__':
